7f6338b03a8467c3b01d4894e33c925c9a1fa4d7
hzume <yota040@gmail.com>
2023-12-29 01:46:26 +0900

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,12 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
   return (
     <html lang="ja">
       <body className={inter.className}>
-        {/* <DataFetch> */}
+        <DataFetch>
           <PageLayout>
             <ModalManager />
             {children}
           </PageLayout>
-        {/* </DataFetch> */}
+        </DataFetch>
       </body>
     </html>
   )
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,8 +1,13 @@
 "use client"
 
 import { signIn } from "next-auth/react"
+import { useEffect } from "react";
 
 export const SignIn = () => {
-    signIn();
+    useEffect(() => {
+        if (typeof window !== 'undefined')
+            signIn();
+    })
+    
     return <></>
 }
